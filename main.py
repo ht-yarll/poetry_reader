81eebc444b1e4bbbc9e8c888abb3f510043883f3
ht-yarll <htorres5@outlook.com>
2025-06-17 12:12:05 -0300

    :test_tube:testing prototype manually

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import easyocr
-import csv
+from google.cloud import vision
 
-def main(image):
-    reader = easyocr.Reader(['pt'])
-    result = reader.readtext(image,decoder='wordbeamsearch', detail=0)
-    print(result)
-    with open(f'data/csvs/{result[0]}.csv', mode='w') as csvfile:
+def test_read_text_from_image(image_path):
+    reader = vision.ImageAnnotatorClient()
 
-        for item in result:
-            writer = csv.writer(csvfile)
-            writer.writerow([item])
+    with open(image_path, "rb") as image_file:
+        content = image_file.read()
 
+    image = vision.Image(content=content)
+    response = reader.document_text_detection(image=image)
+
+    text = response.full_text_annotation.text
+
+    print(f"Detected text:\n")
+    print(text.replace('&', ''))
+
+    # for page in response.full_text_annotation.pages:
+    #     for block in page.blocks:
+    #         for paragraph in block.paragraphs:
+    #             for word in paragraph.words:
+    #                 word_text = ''.join([
+    #                     symbol.text for symbol in word.symbols
+    #                 ])
+    #                 print(f"Word text: {word_text}".format(word_text=word_text))
 
 if __name__ == "__main__":
-    main("data/test.png")
+    test_read_text_from_image("data/20250617_105514.jpg")
